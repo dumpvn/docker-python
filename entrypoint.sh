641da5e2fcfef72d68575d2627fe1623ea103e54
Phuc Nguyen <32007143+dumpvn@users.noreply.github.com>
2021-12-18 13:44:18 +0700

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,5 +4,8 @@ set -euxo pipefail
 
 # shellcheck disable=SC2016
 envsubst '${APP_USER} ${NGINX_WORKER}' < /etc/nginx/nginx.conf | sponge /etc/nginx/nginx.conf
+# envsubst '${APP_USER} ${APP_HOME}' < /etc/supervisor/conf.d/uwsgi.conf | sponge /etc/supervisor/conf.d/uwsgi.conf
+# envsubst '${SERVICE} ${APP_HOME}' < /etc/nginx/conf.d/www.conf | sponge /etc/nginx/conf.d/www.conf
+
 
 exec /usr/bin/supervisord -n -c /etc/supervisor/supervisord.conf
